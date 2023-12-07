ed8ccae85da7b0295b2dfa21ce65e23c11239c85
rdog15 <131314101+rdog15@users.noreply.github.com>
2023-12-07 12:30:03 -0800

    JSON object counter

--- Assign_13_2.py
+++ Assign_13_2.py
@@ -1,6 +1,10 @@
+# It prompts the user to enter a location (URL), retrieves data from the specified URL,
+# It prints the URL and the number of characters retrieved, the retrieved data, assumed to be in JSON format, 
+# is loaded into a Python object, It iterates over the "comments" in the JSON object, extracts the "count" from each comment, 
+# converts it to an integer, and accumulates the sum, it increments the total count for each comment, 
+# finally, it prints the total count and the sum of counts.
 import urllib.request as ur
 import json
-
 
 url = input("Enter location: ")
 print("Retrieving ", url)
