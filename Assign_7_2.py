12bfdf9bfeddb9360f4a94cadc2d2de35fd12412
rdog15 <131314101+rdog15@users.noreply.github.com>
2023-12-07 12:06:45 -0800

    File number average tool

--- Assign_7_2.py
+++ Assign_7_2.py
@@ -1,3 +1,5 @@
+# Prompts user to enter file name, opens file and reads each line, counts how many times
+# "X_Sample_Word" appears, then calculates the average of the numerical values found for each line.
 fname = input("Enter file name: ")
 fh = open(fname)
 
@@ -5,8 +7,8 @@ count = 0
 aver = 0
 
 for line in fh:
-    if line.startswith("X-DSPAM-Confidence:"):
+    if line.startswith("X_Sample_Word"):
         count = count + 1
         total = float(line[line.find(':')+1:])
         aver = aver + total
-print('Average spam confidence:', aver/count)
+print('Average sample words:', aver/count)
